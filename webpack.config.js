b7854aa8bf308df5ed5242b4a0227e08ee2d5cb8
Wilbert Navarrete <wilmion92@gmail.com>
2020-12-29 03:54:04 +0100

    prefeccionando el gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry : path.resolve(__dirname , 'src/index.js'),
     output : {
         path: path.resolve(__dirname , 'dist/'),
-        filename: 'bundle.js'
+        filename: 'js/[name].js'
     },
     module: {
         rules: [
